92489c43f9b92c4e1f2759c0032e74a48fde8cfa
bull0101 <liu.h.y.1201@gmail.com>
2026-09-21 18:35:23 +0800

    Update and rename 學生名單(動態二維陣列-new).cpp to HW1.1 泡泡排序學生名單(動態二維陣列-new).cpp

---
+++ HW1.1 泡泡排序學生名單(動態二維陣列-new).cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
+using namespace std;
+
+bool isonlyEnglish(const char* str) {
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (!::isalpha(static_cast<unsigned char>(str[i]))) {
+            return false;
+        }
+    }
+    return true; 
+}
+
+void bubblesort(char **arr , int size){
+    for(int i = 0 ; i < (size-1) ; i++){
+        for(int j = 0 ; j < (size-i-1) ; j++){
+            
+            // 取得名字的長度
+            int len0 = strlen(arr[j]);
+            int len1 = strlen(arr[j+1]);
+
+            // 利用 長度-1 取得最後一個字元
+            char x0 = arr[j][len0 - 1];
+            char x1 = arr[j+1][len1 - 1];
+
+            // 比較 ASCII 碼並交換「指標」
+            if((int)x0 > (int)x1){
+                char* temp;         // 這裡改成 char*
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+
+    int student_num, max_namelength;
+    char buffer[256];
+
+    cout << "how many students?";
+
+    while(true){      
+        if(cin >> student_num && student_num > 0){
+            break;
+        }
+        else{
+            cin.clear();
+            cin.ignore(10000,'\n');
+
+            cout << "輸入為非正整數 請重新輸入";
+        };
+    };
+
+    cout << "what's the max name length";
+
+    while(true){      
+        if(cin >> max_namelength && max_namelength > 0){
+            break;
+        }
+        else{
+            cin.clear();
+            cin.ignore(10000,'\n');
+
+            cout << "輸入為非正整數 請重新輸入";
+        };
+    };
+
+    char** arr = (char**) malloc(sizeof(char*) * student_num);
+
+    for(int i = 0 ; i < student_num ; i++){
+
+        arr[i] = (char*) malloc(sizeof(char) * (max_namelength + 1));
+    }
+
+    for(int i = 0; i < student_num; i++){
+        cout << "what's your student name? ";
+
+        while(true){
+            cin >> buffer;
+            if(!(isonlyEnglish(buffer))){
+                cout << "非純英文字母 請重新輸入";
+            }
+            else if(strlen(buffer) > max_namelength){
+                cout << "超過字數限制 請重新輸入";
+            }
+            else{
+                break;
+            }
+        };
+        strcpy(arr[i], buffer);
+    };
+
+    bubblesort(arr,student_num);
+
+    cout << " - 學生名單 - " << endl;
+
+    for(int i = 0; i < student_num; i++){
+        
+        cout << "Student " << (i + 1) << ": " << arr[i] << "\n";
+    }
+
+    for (int i = 0; i < student_num; i++) {
+        free(arr[i]);
+    }
+    free(arr);
+    return 0;
+
+};
